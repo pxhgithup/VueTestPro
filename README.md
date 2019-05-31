44f38d9093053382f74368f00bb5b139f77ee113
pxhgithup <874190533@qq.com>
2019-05-31 13:46:02 +0800

    合并代码

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ npm run build --report
 npm run unit
 
 # run e2e tests
-npm run e2e
+npm run e2ehhhhhh
 
 # run all tests
 npm test
--- src/main.js
+++ src/main.js
@@ -2,13 +2,17 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import MintUi from 'mint-ui'
-import 'mint-ui/lib/style.css'
+import 'mint-ui/lib/style.css'      //固定路径
+import './lib/mui/css/mui.min.css'
 import App from './App'
 import router from './router'
-
 Vue.use(MintUi)
 
+
+
+
 Vue.config.productionTip = false
+Vue.config.devtools = true
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
